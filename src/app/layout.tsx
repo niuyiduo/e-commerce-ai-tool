bf9e3d9724c0e0d1dcbee4019111710be2542a21
Nyd666 <495986579@qq.com>
2025-11-27 22:37:33 +0800

    chore: 更名为朵朵素材魔方

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import React, { ReactNode } from "react";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "电商商品素材智能生成工具",
-  description: "基于 AI 的电商素材生成助手",
+  title: "朵朵素材魔方",
+  description: "基于 AI 的电商素材智能生成工具",
 };
 
 export default function RootLayout({
